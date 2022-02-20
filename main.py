1ba0424d72612a5d95ab2cd03a4c816c89198c4b
expoli <31023767+expoli@users.noreply.github.com>
2022-02-20 10:04:07 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
 
 users = []
+# 尝试通过使用尝试次数增加容错率
+retry_time = 10
 
 try:
 
@@ -26,11 +28,25 @@ try:
     #     time.sleep(30)
     for user in users:
         commit_msg = sign_in(user.uid, user.pwd, user.email)
-        if commit_msg != "":
-            msg = user.uid + ": " + commit_msg
-            print("Emailing to User {0} for notification".format(user.uid[-3:]))
-            mail(msg, user.email)
-            print("Emailing is finished")
+        for i in range(0,retry_time):
+            if "已完成今日" in commit_msg:
+                msg = user.uid + ": " + commit_msg
+                print("Emailing to User {0} for notification".format(user.uid[-3:]))
+                mail(msg, user.email)
+                print("Emailing is finished")
+                break
+            elif "今日您已经填报过了" in commit_msg:
+                msg = user.uid + ": " + commit_msg
+                print("Emailing to User {0} for notification".format(user.uid[-3:]))
+                mail(msg, user.email)
+                print("Emailing is finished")
+                break
+            elif i == retry_time-1:
+                msg = user.uid + ": " + commit_msg
+                print("Emailing to User {0} for notification".format(user.uid[-3:]))
+                mail(msg, user.email)
+                print("Emailing is finished")
+                break
 
 except Exception as e:
     print(e.__str__())
